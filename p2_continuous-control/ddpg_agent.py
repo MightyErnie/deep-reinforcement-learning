1df83611638231a30d90bc4dd1c4242fc37de028
Anis Ahmad <a3ahmad@gmail.com>
2018-11-09 12:55:02 -0800

    Add prioritized replay, a faster implementation, separate training to be more like Ape-X, and added basic profiling code

--- p2_continuous-control/ddpg_agent.py
+++ p2_continuous-control/ddpg_agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy_ringbuffer import RingBuffer
 import random
 import copy
 from collections import namedtuple, deque
@@ -9,7 +10,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
+BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
@@ -17,7 +18,14 @@ LR_ACTOR = 1e-4         # learning rate of the actor
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+STARTING_BETA = 0.4
+BETA_STEPS = 10000
+
+REWARD_STEPS = 5
+
+ACTOR_UPDATE_STEPS = 5
+
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
@@ -48,20 +56,20 @@ class Agent():
         # Noise process
         self.noise = OUNoise(num_agents, action_size, random_seed)
         
+        # Prioritized replay annealing
+        self.beta = STARTING_BETA
+        self.beta_step = (1.0 - STARTING_BETA) / BETA_STEPS
+    
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = PrioritizedReplayBuffer(state_size, action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
-    def step(self, state, action, reward, next_state, done):
+    def store_transitions(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
+        
         # Save experience / reward
         self.memory.add(state, action, reward, next_state, done)
 
-        # Learn, if enough samples are available in memory
-        if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
-            self.learn(experiences, GAMMA)
-
-    def act(self, state, add_noise=True):
+    def determine_actions(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
         state = torch.from_numpy(state).float().to(device)
         self.actor_local.eval()
@@ -76,32 +84,45 @@ class Agent():
     def reset(self):
         self.noise.reset()
 
-    def learn(self, experiences, gamma):
+    def learn(self):
         """Update policy and value parameters using given batch of experience tuples.
         Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
         where:
             actor_target(state) -> action
             critic_target(state, action) -> Q-value
-
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
-            gamma (float): discount factor
         """
-        states, actions, rewards, next_states, dones = experiences
+
+        # Learn, if enough samples are available in memory
+        if len(self.memory) < BATCH_SIZE:
+            return
+
+        (states, actions, rewards, next_states, dones), sample_indices, weights = self.memory.sample(self.beta)
+        self.beta += self.beta_step
+        self.beta = max(self.beta, 1.0)
 
         # ---------------------------- update critic ---------------------------- #
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
+
         # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-        # Compute critic loss
+        Q_targets = rewards + (GAMMA * Q_targets_next * (1 - dones))
         Q_expected = self.critic_local(states, actions)
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
+
+        # Compute weighted losses for the prioritized replay
+        loss = (Q_expected - Q_targets) ** 2.0
+        if (weights is not None) and (sample_indices is not None):
+            weights = torch.tensor(weights).float().to(device).unsqueeze(-1)
+            weighted_losses = (weights * loss).detach()
+            self.memory.update_priorities(sample_indices, weighted_losses.cpu().data.numpy())
+
+        # Compute critic loss
+        critic_loss = loss.mean()
+
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+
         torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
         self.critic_optimizer.step()
 
@@ -109,6 +130,7 @@ class Agent():
         # Compute actor loss
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
+
         # Minimize the loss
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
@@ -130,6 +152,16 @@ class Agent():
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+    def fully_update_actor(self, agent):
+        """Copies actor networks from the source
+
+        Params
+        ======
+            agent: Agent providing the new network
+        """
+        for target_param, source_param in zip(self.actor_local.parameters(), agent.actor_local.parameters()):
+            target_param.data.copy_(source_param)
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
@@ -156,7 +188,7 @@ class OUNoise:
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    def __init__(self, state_size, action_size, buffer_size, batch_size, seed):
         """Initialize a ReplayBuffer object.
         Params
         ======
@@ -174,7 +206,7 @@ class ReplayBuffer:
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
     
-    def sample(self):
+    def sample(self, beta):
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
 
@@ -184,8 +216,139 @@ class ReplayBuffer:
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
-        return (states, actions, rewards, next_states, dones)
+        return (states, actions, rewards, next_states, dones), None, None
+
+    def update_priorities(self, indices, new_priorities):
+        pass
 
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
+
+
+class PrioritizedReplayBufferDeque:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, state_size, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.priorities = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+        self.alpha = 0.6
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+        if (len(self.priorities) > 0):
+            self.priorities.append(max(self.priorities))
+        else:
+            self.priorities.append(1.0)
+    
+    def sample(self, beta):
+        # Compute the normalized probabilities
+        probabilities = np.array(self.priorities) ** self.alpha
+        probabilities /= sum(probabilities)
+
+        sample_indices = np.random.choice(len(self.memory), self.batch_size, p=probabilities)
+
+        states = torch.from_numpy(np.vstack([self.memory[i].state for i in sample_indices])).float().to(device)
+        actions = torch.from_numpy(np.vstack([self.memory[i].action for i in sample_indices])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([self.memory[i].reward for i in sample_indices])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([self.memory[i].next_state for i in sample_indices])).float().to(device)
+        dones = torch.from_numpy(np.vstack([self.memory[i].done for i in sample_indices]).astype(np.uint8)).float().to(device)
+
+        weights = (len(probabilities) * probabilities[sample_indices]) ** (-beta)
+        weights /= weights.max()
+
+        return (states, actions, rewards, next_states, dones), sample_indices, weights
+
+    def update_priorities(self, indices, new_priorities):
+        new_priorities = new_priorities.reshape(new_priorities.shape[0])
+        for idx, prio in zip(indices, new_priorities):
+            self.priorities[idx] = prio
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+class PrioritizedReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, state_size, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        self.buffer_size = buffer_size
+        self.used_size = 0
+        self.next_entry = 0
+
+        self.states = np.zeros((buffer_size, state_size), dtype=np.float32)
+        self.actions = np.zeros((buffer_size, action_size), dtype=np.float32)
+        self.rewards = np.zeros(buffer_size, dtype=np.float32)
+        self.next_states = np.zeros((buffer_size, state_size), dtype=np.float32)
+        self.dones = np.zeros(buffer_size, dtype=np.float32)
+        self.priorities = np.ones(buffer_size, dtype=np.float32)
+
+        self.batch_size = batch_size
+        self.seed = random.seed(seed)
+        self.alpha = 0.6
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        # ANIS TODO: Handle adding batches at once
+        self.states[self.next_entry] = state
+        self.actions[self.next_entry] = action
+        self.rewards[self.next_entry] = reward
+        self.next_states[self.next_entry] = next_state
+        self.dones[self.next_entry] = done
+
+        if self.used_size != 0:
+            self.priorities[self.next_entry] = np.max(self.priorities[:self.used_size])
+
+        self.next_entry = (self.next_entry + 1) % self.buffer_size
+        self.used_size = min(self.used_size + 1, self.buffer_size)
+
+    def generate_sampling_probabilities(self):
+        probabilities = np.power(self.priorities[:self.used_size], self.alpha)
+        probabilities /= np.sum(probabilities)
+        return probabilities
+    
+    def sample(self, beta):
+        # Compute the normalized probabilities
+        probabilities = self.generate_sampling_probabilities()
+
+        sample_indices = np.random.choice(self.used_size, self.batch_size, p=probabilities)
+
+        states = torch.from_numpy(np.vstack([self.states[i] for i in sample_indices])).float().to(device)
+        actions = torch.from_numpy(np.vstack([self.actions[i] for i in sample_indices])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([self.rewards[i] for i in sample_indices])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([self.next_states[i] for i in sample_indices])).float().to(device)
+        dones = torch.from_numpy(np.vstack([self.dones[i] for i in sample_indices]).astype(np.uint8)).float().to(device)
+
+        weights = np.power(self.used_size * probabilities[sample_indices], -beta)
+        weights /= weights.max()
+
+        return (states, actions, rewards, next_states, dones), sample_indices, weights
+
+    def update_priorities(self, indices, new_priorities):
+        new_priorities = new_priorities.reshape(new_priorities.shape[0])
+        for idx, prio in zip(indices, new_priorities):
+            self.priorities[idx] = prio
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return self.used_size
--- p2_continuous-control/train.py
+++ p2_continuous-control/train.py
@@ -3,8 +3,18 @@ import numpy as np
 import torch
 from collections import deque
 import matplotlib.pyplot as plt
+import threading
+import ddpg_agent
 
+from cProfile import Profile
+from pstats import Stats
+
+transitions = deque(maxlen=ddpg_agent.REWARD_STEPS)
+
+#env = UnityEnvironment(file_name='./Reacher_Windows_x86_64/Reacher.exe')
 env = UnityEnvironment(file_name='./Reacher_Windows_x86_64_20/Reacher.exe')
+#env = UnityEnvironment(file_name='/data/Reacher_One_Linux_NoVis/Reacher_One_Linux_NoVis.x86_64')
+#env = UnityEnvironment(file_name='/data/Reacher_Linux_NoVis/Reacher.x86_64')
 
 # get the default brain
 brain_name = env.brain_names[0]
@@ -24,51 +34,92 @@ actions = np.zeros((num_agents, action_size))
 states = env_info.vector_observations
 state_size = states.shape[1]
 
-from ddpg_agent import Agent
-agent = Agent(num_agents=num_agents, state_size=state_size, action_size=action_size, random_seed=10)
+learner = ddpg_agent.Agent(num_agents=num_agents, state_size=state_size, action_size=action_size, random_seed=10)
+actor = ddpg_agent.Agent(num_agents=num_agents, state_size=state_size, action_size=action_size, random_seed=0)
 
-def ddpg(n_episodes=200, max_t=700):
+def tick_simulation(actions):
+    global env
+    env_info = env.step(actions)[brain_name]
+    return env_info.vector_observations, env_info.rewards, env_info.local_done
+
+def plot_scores(scores, mean_scores):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)    
+    plt.plot(np.arange(1, len(scores)+1), scores)
+    plt.plot(np.arange(1, len(mean_scores)+1), mean_scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.savefig("score_plot.png", dpi=600)
+    plt.close(fig)
+
+def tick_learning():
+    learner.learn()
+
+def ddpg(n_episodes=2000, max_t=700):
     scores_deque = deque(maxlen=100)
     scores = []
+    mean_scores = []
     max_score = -np.Inf
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset()[brain_name]
         states = env_info.vector_observations
-        agent.reset()
+        learner.reset()
         score = 0
+
         for t in range(max_t):
             # Request new actions for each agent
-            actions = agent.act(states)
-            
+            actions = actor.determine_actions(states)   # TODO: Switch to using the actor
+        
             # Step the environment and retrieve the new states, rewards and whether or not the agent is done
-            env_info = env.step(actions)[brain_name]
-            next_states = env_info.vector_observations
-            rewards = env_info.rewards
-            dones = env_info.local_done
+            next_states, rewards, dones = tick_simulation(actions)
 
-            #for idx, (state, action, reward, next_state, done) in enumerate(zip(states, actions, rewards, next_states, dones)):
-            #    agents[idx].step(np.copy(state), np.copy(action), np.copy(reward), np.copy(next_state), np.copy(done))
+            # Store all these transitions into the replay buffer
+            for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+                learner.store_transitions(np.copy(state), np.copy(action), np.copy(reward), np.copy(next_state), np.copy(done))
+
+            # Learn from a minibatch of transitions
+            tick_learning()
+
+            # Periodically update the actor
+            if (t % ddpg_agent.ACTOR_UPDATE_STEPS) == 0:
+                actor.fully_update_actor(learner)
 
             states = next_states
             score += sum(rewards) / len(rewards)
             if False not in dones:
                 break 
+
         scores_deque.append(score)
         scores.append(score)
+        mean_scores.append(np.mean(scores_deque))
         print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
-        if i_episode % 100 == 0:
-            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+        plot_scores(scores, mean_scores)
+        if i_episode % 10 == 0:
+            torch.save(learner.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(learner.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+        if (i_episode > 100) and (np.mean(scores_deque) > 30.0):
+            torch.save(learner.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(learner.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+            return scores
     return scores
+
+# Optional profiling step (just a handful of episodes)
+if True:
+    profiler = Profile()
+    profiler.runcall(ddpg, 3, 700)
+
+    stats = Stats(profiler)
+    stats.strip_dirs()
+    stats.sort_stats('cumulative')
+    stats.print_stats()
+
+    env.close()
+    exit()
 
 scores = ddpg()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(1, len(scores)+1), scores)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-plt.savefig("score_plot.png", dpi=600)
+plot_scores(scores)
 
 env.close()
